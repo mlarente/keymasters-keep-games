ac1b76a89f644f538d9d168a2a6a4e9ff52965ee
Martin Larente <martin.larente@gmail.com>
2026-09-17 21:05:45 -0400

    fix(catherine): improve customizability

--- catherine_full_body.py
+++ catherine_full_body.py
@@ -3,8 +3,9 @@ from __future__ import annotations
 from typing import List
 
 from dataclasses import dataclass
+import itertools
 
-from Options import OptionError, OptionSet
+from Options import OptionError, OptionSet, Range
 
 from ..game import Game
 from ..game_objective_template import GameObjectiveTemplate
@@ -16,6 +17,8 @@ from ..enums import KeymastersKeepGamePlatforms
 class CatherineOptions:
     catherine_full_body_difficulties: CatherineDifficulties
     catherine_full_body_babel_stages: CatherineBabelStages
+    catherine_full_body_maximum_rapunzel_stage: CatherineMaximumRapunzelStage
+    catherine_full_body_maximum_rapunzel_extra_stage: CatherineMaximumRapunzelExtraStage
 
 class Catherine(Game):
     name = "Catherine: Full Body"
@@ -27,7 +30,7 @@ class Catherine(Game):
     options_cls = CatherineOptions
 
     def game_objective_templates(self) -> List[GameObjectiveTemplate]:
-        return [
+        templates: List[GameObjectiveTemplate] = [
             GameObjectiveTemplate(
                 label="Golden Playhouse: Reach the ENDING ending in MODE mode on DIFFICULTY difficulty",
                 data={
@@ -38,14 +41,9 @@ class Catherine(Game):
                 is_time_consuming=True,
                 weight=2,
             ),
-            GameObjectiveTemplate(
-                label="Golden Playhouse: Beat Rapunzel stage STAGE",
-                data={
-                    "STAGE": (lambda: [i for i in range(1, 16)], 1),
-                },
-                weight=7,
-            ),
-            GameObjectiveTemplate(
+        ]
+        if self.self.golden_playhouse_difficulties():
+            templates.append(GameObjectiveTemplate(
                 label="Golden Playhouse: Get at least RANK rank in the day DAY nightmare in MODE mode on DIFFICULTY difficulty",
                 data={
                     "RANK": (self.ranks, 1),
@@ -54,15 +52,25 @@ class Catherine(Game):
                     "DIFFICULTY": (self.golden_playhouse_difficulties, 1),
                 },
                 weight=10
-            ),
-            GameObjectiveTemplate(
+            ))
+        if self.babel_stages():
+            templates.append(GameObjectiveTemplate(
                 label="Babel: Beat the BABEL_STAGE",
                 data={
                     "BABEL_STAGE": (self.babel_stages, 1),
                 },
                 weight=5
-            ),
-        ]
+            ))
+        if self.rapunzel_stages():
+            templates.append(GameObjectiveTemplate(
+                label="Golden Playhouse: Beat Rapunzel STAGE",
+                data={
+                    "STAGE": (self.rapunzel_stages, 1),
+                },
+                weight=7,
+            ))
+
+        return templates
 
     def difficulties(self) -> List[str]:
         difficulties = list(self.archipelago_options.catherine_full_body_difficulties.value)
@@ -74,10 +82,7 @@ class Catherine(Game):
         return list(self.archipelago_options.catherine_full_body_babel_stages.value)
 
     def golden_playhouse_difficulties(self) -> List[str]:
-        difficulties = [d for d in self.difficulties() if d != "Safety"]
-        if not difficulties:
-            return ["Easy"]
-        return difficulties
+        return list(d for d in self.difficulties() if d != "Safety")
 
     def modes(self) -> List[str]:
         return ["Classic", "Remix"]
@@ -102,9 +107,26 @@ class Catherine(Game):
     def ranks(self) -> List[str]:
         return ["Bronze", "Silver", "Gold"]
 
+    def rapunzel_stages(self) -> List[str]:
+        max_stage = self.archipelago_options.catherine_full_body_maximum_rapunzel_stage.value
+        max_extra_stage = self.archipelago_options.catherine_full_body_maximum_rapunzel_extra_stage.value
+
+        return list(itertools.chain(
+            (f"stage {i}" for i in range(1, max_stage + 1)),
+            (f"extra stage {i}" for i in range(1, max_extra_stage + 1)),
+        ))
+
 class CatherineDifficulties(OptionSet):
     """
-    Difficulties that may be used in challenges.
+    Difficulties that may be used in objectives.
+
+    Valid values:
+    - Safety
+    - Easy
+    - Normal
+    - Hard
+
+    Note that enabling only Safety will disable nightmare objectives.
     """
     display_name = "Allowed difficulties"
     valid_keys = [
@@ -117,7 +139,14 @@ class CatherineDifficulties(OptionSet):
 
 class CatherineBabelStages(OptionSet):
     """
-    Babel stages that may be used in challenges.
+    Babel stages that may be used in objectives.
+    Use an empty list to disable Babel stage objectives.
+
+    Valid values:
+    - Altar
+    - Menhir
+    - Obelisk
+    - Axis Mundi
     """
     display_name = "Babel stages"
     valid_keys = [
@@ -131,3 +160,18 @@ class CatherineBabelStages(OptionSet):
         "Menhir",
         "Obelisk",
     ]
+
+class CatherineMaximumRapunzelStage(Range):
+    """
+    Highest Rapunzel stage that may be included in objectives.
+    Set to zero to disable Rapunzel stage objectives.
+    """
+    range_end = 64
+    default = 15
+
+class CatherineMaximumRapunzelExtraStage(Range):
+    """
+    Highest Rapunzel extra stage that may be included in objectives.
+    Set to zero to disable Rapunzel extra stage objectives.
+    """
+    range_end = 64
